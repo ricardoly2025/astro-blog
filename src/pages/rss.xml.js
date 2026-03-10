1d59acc306e186901a8bc3b83cbed2a7e675e423
ricardoly2025 <ricardoly2025@gmail.com>
2026-03-10 14:21:27 -0300

    RSS Feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,17 @@
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+
+export async function GET(context){
+
+const posts = await getCollection('blog')
+
+return rss({
+title: 'Meu Blog',
+site: context.site,
+items: posts.map(post => ({
+title: post.data.title,
+link: `/post/${post.slug}`
+}))
+})
+
+}
